4d917292d5138c2cbaeae7ba4ec4042d803b7d30
Leonardo <poliveira.leonardo@gmail.com>
2022-09-28 21:21:33 -0300

    Primeiro Commit

---
+++ ConsumindoSOAP/Correios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumindoSOAP
+{
+
+    public class Cep
+    {
+        [Required]
+        [MaxLength(9, ErrorMessage = "Cep tem que ser digitado no formato 70000-000")]
+        [MinLength(9, ErrorMessage = "Cep tem que ser digitado no formato 70000-000")]
+        public string Codigo { get; set; }
+    }
+    public class Endereco
+    {
+        public string Descricao { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+    }
+
+}
---
+++ ConsumindoSOAP/Program.cs
@@ -0,0 +1,30 @@
+﻿using static ServicoDePaises.CountryInfoServiceSoapTypeClient;
+
+namespace ConsumindoSOAP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var request = new ServicoDePaises.CountryInfoServiceSoapTypeClient(EndpointConfiguration.CountryInfoServiceSoap12))
+            {
+                var paises = request.ListOfCountryNamesByCode();
+
+                foreach(var pais in paises)
+                {
+                    var capitalDoPais = request.CapitalCity(pais.sISOCode);
+
+                    var DDI = request.CountryIntPhoneCode(pais.sISOCode);
+
+                    Console.WriteLine($"Pais: {pais.sName} - Capital do Pais: {capitalDoPais} - DDI: +{DDI}");
+                }
+
+                Console.ReadLine();
+
+            }
+        }
+
+    }
+}
+
+
